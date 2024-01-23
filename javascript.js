53263712f0c0717a40cf959aeaf8fbc6340739f9
MudWallow <153695981+MudWallow@users.noreply.github.com>
2024-01-22 20:31:05 -0700

    Change input to prompt and add score counters to make the game fully playable.

--- javascript.js
+++ javascript.js
@@ -1,8 +1,48 @@
-let playerChoice = "Paper";
+let playerChoice;
 let computerChoice;
-const gameText = document.querySelector('.gameText');
-const playerInput = document.querySelector('#playerInput');
-const submit = document.querySelector('.submit');
+let wins = 0;
+let enemyWins = 0;
+const winTitle = document.querySelector('#winTitle');
+const startGame = document.querySelector('.startGame');
+const winScore = document.querySelector('#winScore');
+const enemyWinScore = document.querySelector('#enemyWinScore');
+
+startGame.addEventListener("click", playRound);
+
+function compare () {
+    playerChoice = playerChoice.toLowerCase();
+    if (playerChoice === computerChoice) {
+        alert ("You both chose " + playerChoice + "! It's a draw.")
+    } else if (playerChoice === "paper" && computerChoice === "rock") {
+        ++wins
+        alert ("You chose " + playerChoice + ", they chose " + computerChoice + "! You win!")
+    } else if (playerChoice === "paper" && computerChoice === "scissors") {
+        ++enemyWins
+        alert ("You chose " + playerChoice + ", they chose " + computerChoice + "! You lose!")
+    } else if (playerChoice == "scissors" && computerChoice === "paper") {
+        ++wins
+        alert ("You chose " + playerChoice + ", they chose " + computerChoice + "! You win!")
+    } else if (playerChoice == "scissors" && computerChoice === "rock") {
+        ++enemyWins
+        alert ("You chose " + playerChoice + ", they chose " + computerChoice + "! You lose!")
+    } else if (playerChoice === "rock" && computerChoice === "paper") {
+        ++enemyWins
+        alert ("You chose " + playerChoice + ", they chose " + computerChoice + "! You lose!")
+    } else if (playerChoice === "rock" && computerChoice === "scissors") {
+        ++wins
+        alert ("You chose " + playerChoice + ", they chose " + computerChoice + "! You win!")
+    } else {
+        alert ("Type 'rock', 'paper', or 'scissors'.");
+    }
+}
+
+
+function playRound() {
+    playerChoice = prompt("Choose rock, paper, or scissors!");
+    getComputerChoice();
+    compare();
+    game();
+}
 
 function getComputerChoice() {
     const options = ["rock", "paper", "scissors"];
@@ -11,26 +51,8 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-function compare() {
-    playerChoice = playerChoice.toLowerCase();
-    if (playerChoice === computerChoice) {
-        gameText.textContent = ("You both chose " + playerChoice + "! It's a draw.")
-    } else if (playerChoice === "paper" && computerChoice === "rock") {
-        gameText.textContent = ("You chose " + playerChoice + ", they chose " + computerChoice + "! You win!")
-    } else if (playerChoice === "paper" && computerChoice === "scissors") {
-        gameText.textContent = ("You chose " + playerChoice + ", they chose " + computerChoice + "! You lose!")
-    } else if (playerChoice == "scissors" && computerChoice === "paper") {
-        gameText.textContent = ("You chose " + playerChoice + ", they chose " + computerChoice + "! You win!")
-    } else if (playerChoice == "scissors" && computerChoice === "rock") {
-        gameText.textContent = ("You chose " + playerChoice + ", they chose " + computerChoice + "! You lose!")
-    } else if (playerChoice === "rock" && computerChoice === "paper") {
-        gameText.textContent = ("You chose " + playerChoice + ", they chose " + computerChoice + "! You lose!")
-    } else if (playerChoice === "rock" && computerChoice === "scissors") {
-        gameText.textContent = ("You chose " + playerChoice + ", they chose " + computerChoice + "! You win!")
-    } else {
-        gameText.textContent = (playerChoice);
-    }
+function game() {
+    winScore.textContent = (wins);
+    console.log(wins);
+    enemyWinScore.textContent = (enemyWins);
 }
-
-getComputerChoice();
-compare();
